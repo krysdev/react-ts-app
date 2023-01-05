610b86d3b20701193c3ed03971118ca39384bd09
KrysDev <webdevkrys@gmail.com>
2023-01-05 20:36:06 +0000

    x

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import List from './state/List';
+// import List from './state/List';
+import UserSearch from './state/UserSearch';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <List />
+    {/* <List /> */}
+    <UserSearch/>
   </React.StrictMode>
 );
--- src/state/List.tsx
+++ src/state/List.tsx
@@ -2,17 +2,28 @@ import { useState } from 'react';
 
 const List: React.FC = () => {
   const [name, setName] = useState('');
+  const [list, setList] = useState<string[]>([]);
+
+  const onClick = () => {
+    setName('');
+    setList([...list, name]);
+  };
 
   return (
     <div>
       <h3>List</h3>
+      <ul>
+        {list.map((el) => (
+          <li key={el}>{el}</li>
+        ))}
+      </ul>
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <br />
-      <button>ADD name</button>
+      <button onClick={onClick}>ADD name</button>
     </div>
   );
 };
